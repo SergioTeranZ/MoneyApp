e39c024e5146c927469f8816c838f4b8e86cf44f
sergio <sergioteran.zambrano@gmail.com>
2016-12-14 14:04:46 -0400

    AVE MARIA PURISIMA YA HACE TRANSFERENCIAS DIOS MIO SANTO

--- src/pages/confirmacion/confirmacion.ts
+++ src/pages/confirmacion/confirmacion.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import firebase from 'firebase';
 
 /*
   Generated class for the Confirmacion page.
@@ -15,16 +17,45 @@ export class ConfirmacionPage {
 	public userProfile : any;
 	public userSelected : any;
 	public valor : any;
+	public userRef: any;
 
   constructor(public nav: NavController,public navParams: NavParams) {
   	this.nav = nav;
   	this.userProfile = navParams.get('userProfile');
     this.userSelected = navParams.get('userSelected');
     this.valor = navParams.get('valor');
+
+    this.userRef = firebase.database().ref('/userProfile');
+
+		this.userRef.on('value', usersList => {
+		  let users = [];
+		  usersList.forEach( user => {
+		  	if(user.val().email == this.userSelected.email){this.userSelected = user.val(); this.userSelected.key = user.getKey()};
+		  	if(user.val().email == this.userProfile.email) {this.userProfile  = user.val(); this.userProfile.key  = user.getKey()};
+		  });
+		});
   }
 
   ionViewDidLoad() {
     console.log('Hello ConfirmacionPage Page');
   }
+
+	cancelTransfer(){
+    this.nav.setRoot(HomePage); 	
+	}
+
+  proceedTranfer(){
+
+  	var saldoSele : any = +this.valor + this.userSelected.saldo;
+  	var saldoCurr : any =  this.userProfile.saldo  - this.valor;
+  	
+  	saldoCurr = String(saldoCurr);
+  	saldoSele = String(saldoSele);
+
+  	this.userRef.child(this.userProfile.key).update({ saldo: saldoCurr, });
+  	this.userRef.child(this.userSelected.key).update({ saldo: saldoSele, });
+    
+    this.nav.setRoot(HomePage);	
+ 	} 
 
 }
